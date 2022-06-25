a95bc81a1dc665c69c0cf8065ed65334ec9e96b0
opensesa0508 <opensea0508@gmail.com>
2022-06-25 14:02:02 -0500

    chore: installed node_modules and modified dbconfig

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
   host: "db-mysql-backgammon-99999-do-user-11834163-0.b.db.ondigitalocean.com",
   user: "doadmin",
   password: "AVNS_m4GblO-IQda4KisB8hn",
-  database: "db-mysql-backgammon-99999",
+  database: "defaultdb",
   port: "25060",
   dialect: "mysql",
   pool: {
