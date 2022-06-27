cc0949dcf10ba378a031b58f81fd25a07107a645
opensesa0508 <opensea0508@gmail.com>
2022-06-27 14:10:11 -0500

    chore:inesrt server.on

--- index.ts
+++ index.ts
@@ -274,6 +274,9 @@ export function afterGuestOut(removeId: string | undefined) {
     }
   }
 }
+server.on('listening',function(){
+  console.log('ok, server is running');
+});
 
 server.listen(PORT, () => {
   console.log(`The application is listening on port ${PORT}!`);
